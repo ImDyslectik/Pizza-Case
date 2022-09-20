0455894adbbc1269b083b1e15b80949914735904
ImDyslectik <robin.alkema@student.nhlstenden.com>
2022-09-20 11:11:37 +0200

    update inputvalidator

--- src/InputValidationPattern.java
+++ src/InputValidationPattern.java
@@ -1,10 +1,16 @@
 public class InputValidationPattern {
     public boolean Validator(String Name, String StreetName, String PostalAdress) {
-        if (Name.isEmpty() || StreetName.isEmpty() || PostalAdress.isEmpty()) {
+        if (Name.isEmpty() || StreetName.isEmpty()) {
             System.out.println("het lijkt erop dat u iets leeg gelaten heeft");
             return false;
+
         } else {
-            return true;
+            if (PostalAdress.matches("[1-9]{1}[0-9]{3}[a-zA-Z]{2}")) {
+                return true;
+            }
+            System.out.println("het lijkt dat uw postcode niet bestaat ???");
+            return false;
         }
     }
+
 }
