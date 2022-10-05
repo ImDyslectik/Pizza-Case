4962f4ae4a298fe918e2063b2d2bdfdd66384ea9
ImDyslectik <robin.alkema@student.nhlstenden.com>
2022-10-05 11:15:53 +0200

    updated version

--- src/Client.java
+++ src/Client.java
@@ -5,14 +5,14 @@ import java.io.*;
 public class Client {
     // initialize socket and input output streams
     private Socket socket = null;
-    private DataInputStream input = null;
-    private DataOutputStream out = null;
+    private static Client instance;
 
     // constructor to put ip address and port
     // establish a connection
 
     public Client(String address, int port) {
         try {
+<<<<<<< Updated upstream
             while (true) {
                 socket = new Socket(address, port);
                 System.out.println("Connected");
@@ -21,19 +21,25 @@ public class Client {
                 LinkedList<String> get = MyOrder.getOrder();
                 LinkedList<String> message = encrypter.encryptList(get);
                 // takes input from terminal, this needs to be changed to input from the GUI
+=======
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+            OrderManager MyOrder = new OrderManager();
+            MyOrder.Bestel();
+            LinkedList<String> get = MyOrder.getOrder();
+            LinkedList<String> message = Encrypter.encrypt(get);
+            // takes input from terminal, this needs to be changed to input from the GUI
+>>>>>>> Stashed changes
 
-                OutputStream outputStream = socket.getOutputStream();
-                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-                objectOutputStream.writeObject(message);
-            }
+            OutputStream outputStream = socket.getOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject(message);
         } catch (UnknownHostException u) {
             System.out.println("Unknown host,zet je internet effe aan ofzo");
         } catch (IOException i) {
             System.out.println("There is a failure during reading, writing");
         } finally {
             try {
-                input.close();
-                out.close();
                 socket.close();
             } catch (IOException i) {
                 System.out.println(i);
