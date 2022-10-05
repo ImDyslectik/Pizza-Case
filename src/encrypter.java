acce47142748355ffffcbc044428faf76fa80bc6
KevinHerrema <kevin.herrema@student.nhlstenden.com>
2022-10-05 11:56:42 +0200

    encrypted comments

--- src/encrypter.java
+++ src/encrypter.java
@@ -13,16 +13,16 @@ public interface encrypter {
     final String secretKey = "Dyslecthick";
 
     /**
-     * 
-     * @param myKey
-     * @return
+     * makes a key for encryption of data from a given seed
+     * @param myKey the seed for the creation of key
+     * @return the encrytion key
      */
 
-    public static SecretKeySpec prepareSecreteKey(String myKey) {
+    public static SecretKeySpec prepareSecreteKey(String seed) {
         byte[] key;
         MessageDigest sha = null;
         try {
-            key = myKey.getBytes(StandardCharsets.UTF_8);
+            key = seed.getBytes(StandardCharsets.UTF_8);
             sha = MessageDigest.getInstance("SHA-1");
             key = sha.digest(key);
             key = Arrays.copyOf(key, 16);
@@ -35,9 +35,9 @@ public interface encrypter {
     }
 
     /**
-     * 
-     * @param order
-     * @return
+     * encrypts a list of strings
+     * @param order list of strings to encrypt
+     * @return encrypted list of strings
      */
     public static LinkedList<String> encryptList(LinkedList<String> order) {
         LinkedList<String> result = new LinkedList<>();
@@ -49,10 +49,10 @@ public interface encrypter {
     }
 
     /**
-     * 
-     * @param strToEncrypt
-     * @param secret
-     * @return
+     * encrypts a single string
+     * @param strToEncrypt string that needs to be encrypted
+     * @param secret seed for the creation of encrytion key
+     * @return encrypted string
      */
     public static String encrypt(String strToEncrypt, String secret) {
         try {
@@ -67,9 +67,9 @@ public interface encrypter {
     }
 
     /**
-     * 
-     * @param order
-     * @return
+     * decrypts a list of strings
+     * @param order list of strings that need to be decrypted
+     * @return decrypted list of strings
      */
     public static LinkedList<String> decryptList(LinkedList<String> order) {
         LinkedList<String> result = new LinkedList<>();
@@ -81,10 +81,10 @@ public interface encrypter {
     }
 
     /**
-     * 
-     * @param strToDecrypt
-     * @param secret
-     * @return
+     *decrypts a single string with a given seed 
+     * @param strToDecrypt string that needs to be decrypted
+     * @param secret seed used for creating the decryption key
+     * @return decrypted string
      */
     public static String decrypt(String strToDecrypt, String secret) {
         try {
