10641f042bcae68a58b6db9b3180541480eb4dbe
ImDyslectik <robin.alkema@student.nhlstenden.com>
2022-10-05 11:15:53 +0200

    fuck you

--- src/Server.java
+++ src/Server.java
@@ -7,33 +7,28 @@ public class Server {
     private static Server instance;
     private Socket socket = null;
     private ServerSocket server = null;
-    private DataInputStream in = null;
     LinkedList<String> orders = new LinkedList<>();
     LinkedList<String> rawinput = new LinkedList<>();
 
     /**
-     * constructor for the singleton
-     * starts server and waits for a connection with the client
+     * Constructor for the singleton
+     * Starts server and waits for a connection with the client
      * 
      * @param port is passed on from Main.java containing the port where the Server,
      *             And the Client will be run on
+     *             InputStream gets the input from the Client
      */
     public Server(int port) {
         try {
+            server = new ServerSocket(port);
+            System.out.println("Server Booting Up Awaiting Connection");
 
             while (true) {
-                server = new ServerSocket(port);
-                System.out.println("Server Booting Up Awaiting Connection");
-
-                socket = server.accept();
-                System.out.println("Connected Succesfull");
-
-                // takes input from the client terminal and prints it into the server terminal
-                InputStream inputStream = socket.getInputStream();
-                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
                 try {
-                    rawinput = (LinkedList<String>) objectInputStream.readObject();
+                    socket = server.accept();
+                    System.out.println("Connected Succesfull");
 
+<<<<<<< Updated upstream
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 }
@@ -47,17 +42,31 @@ public class Server {
                 }
                 for (String or : orders) {
                     System.out.println(or);
+=======
+                    // takes input from the client terminal and prints it into the server terminal
+                    InputStream inputStream = socket.getInputStream();
+                    ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                    try {
+                        rawinput = (LinkedList<String>) objectInputStream.readObject();
+
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        orders = Encrypter.decrypt(rawinput);
+                    } catch (Exception e) {
+                        System.out.println("There was a failure during decrypting");
+                    }
+                    for (String or : orders) {
+                        System.out.println(or);
+                    }
+                } catch (IOException i) {
+                    System.out.println("There is a failure during reading, writing");
+>>>>>>> Stashed changes
                 }
             }
         } catch (IOException i) {
             System.out.println("There is a failure during reading, writing");
-        } finally {
-            try {
-                in.close();
-                socket.close();
-            } catch (IOException i) {
-                System.out.println(i);
-            }
         }
     }
 
